e8e575fe9054a8b42004fef3db30893a352dc389
Nosra AHJAOU <noahjaou@gmail.com>
2026-04-25 12:10:55 +0200

    Update fs.h

--- include/fs.h
+++ include/fs.h
@@ -1,1 +1,28 @@
+#ifndef FS_H
+#define FS_H
 
+#include <stdlib.h>
+#include <string.h>
+
+// Structure pour un fichier
+typedef struct File {
+    char name[100];
+    int size;
+    char* content;
+    struct File* next; // Pour lister plusieurs fichiers dans un dossier
+} File;
+
+// Structure pour un répertoire
+typedef struct Directory {
+    char name[100];
+    struct Directory* sub_dirs; // Liste des sous-dossiers
+    struct File* files;         // Liste des fichiers
+    struct Directory* parent;   // Pour pouvoir faire "cd .."
+    struct Directory* next;     // Pour lister les dossiers frères
+} Directory;
+
+// Prototypes des fonctions que nous allons créer
+Directory* create_directory(char* name, Directory* parent);
+void list_content(Directory* current);
+
+#endif
