93cbdf1d7b37da6938728a5e077540d770e5998c
Nosra AHJAOU <noahjaou@gmail.com>
2026-04-25 12:30:05 +0200

    Update huffman.h

--- include/huffman.h
+++ include/huffman.h
@@ -1,10 +1,17 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
 
+#include <stdio.h>
+
+// Structure d'un nœud de l'arbre
 typedef struct Node {
     unsigned char character;
     int frequency;
     struct Node *left, *right;
 } Node;
+
+// Prototypes
+void analyser_frequences(char* nomFichier, int* tableFrequences);
+Node* create_node(unsigned char c, int freq);
 
 #endif
