705b22bee5b2189432e613d966213c918283c431
Nosra AHJAOU <noahjaou@gmail.com>
2026-04-25 12:14:18 +0200

    Update huffman.c

--- src/huffman.c
+++ src/huffman.c
@@ -1,1 +1,24 @@
+#include "huffman.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+// Étape 1 : Analyser le fichier pour compter les fréquences
+void analyser_frequences(char* nomFichier, int* tableFrequences) {
+    FILE* f = fopen(nomFichier, "r");
+    if (!f) {
+        printf("Erreur lors de l'ouverture du fichier.\n");
+        return;
+    }
+
+    int c;
+    while ((c = fgetc(f)) != EOF) {
+        tableFrequences[(unsigned char)c]++;
+    }
+
+    fclose(f);
+}
+
+// Les prochaines étapes seront :
+// - Construire l'arbre de Huffman
+// - Générer les codes binaires
+// - Compresser le fichier
